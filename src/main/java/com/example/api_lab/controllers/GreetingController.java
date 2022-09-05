ed0a55b5cd5e49c9042cbf32b85fbe77c638ba34
Kat Fagg <katjfagg@gmail.com>
2022-09-05 16:01:42 +0100

    requestparam added

--- src/main/java/com/example/api_lab/controllers/GreetingController.java
+++ src/main/java/com/example/api_lab/controllers/GreetingController.java
@@ -1,5 +1,6 @@
 package com.example.api_lab.controllers;
 
+import com.example.api_lab.models.Greeting;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -9,8 +10,13 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping(value = "/greeting") //accepts requests to this path
 public class GreetingController {
 
+//    @GetMapping
+//    public String greeting(@RequestParam(value = "name") String name) {
+//        return "Good afternoon " + name + "!";
+//    }
+
     @GetMapping
-    public String greeting(@RequestParam(value = "name") String name) {
-        return "Good afternoon " + name + "!";
+    public Greeting newGreeting(@RequestParam(value = "timeOfDay") String timeOfDay) {
+        return new Greeting("Kat",timeOfDay);
     }
 }
